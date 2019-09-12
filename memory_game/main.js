2e4ae3c9ba9ee7aac4c8c057e89926233a06e2fa
Brad Donakowski <braddonako@gmail.com>
2019-09-12 11:47:04 -0600

    create game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,9 +1,19 @@
 console.log("Up and running!");
 
-var cardOne = "Queen"
-var cardTwo = "Queen"
-var cardThree = "King"
-var cardFour = "King"
+var cards = ["Queen", "Queen", "King", "King"]; 
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped a Queen');
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped a King");
+
+if (cardsInPlay.length === 2)
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!"); 
+} else {
+	alert("Not a match! Try again.");
+}
